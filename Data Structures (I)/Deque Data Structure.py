9ac4b7f16356f621bb42391d6d89d40482b551ed
Nur2424 <taabaldievnur06@gmail.com>
2025-05-08 16:42:05 +0200

    Create Deque Data Structure.py

---
+++ Data Structures (I)/Deque Data Structure.py
@@ -0,0 +1,56 @@
+# Deque Data Structure
+
+"""
+Deque or Double Ended Queue is a type of queue in which insertion and removal of elements can either be performed 
+from the front or the rear. Thus, it does not follow FIFO rule (First In First Out).
+
+"""
+
+# Deque implementaion in python
+
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addRear(self, item):
+        self.items.append(item)
+
+    def addFront(self, item):
+        self.items.insert(0, item)
+
+    def removeFront(self):
+        return self.items.pop(0)
+
+    def removeRear(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
+d = Deque()
+print(d.isEmpty())
+d.addRear(8)
+d.addRear(5)
+d.addFront(7)
+d.addFront(10)
+print(d.size())
+print(d.isEmpty())
+d.addRear(11)
+print(d.removeRear())
+print(d.removeFront())
+d.addFront(55)
+d.addRear(45)
+print(d.items)
+
+"""
+Applications of Deque Data Structure:
+  ° In undo operations on software.
+  ° To store history in browsers.
+  ° For implementing both stacks and queues.
+
+"""
+
