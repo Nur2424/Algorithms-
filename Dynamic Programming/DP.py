bf0d65937eb7718a05556dd682272a6a37ab177b
Nur2424 <taabaldievnur06@gmail.com>
2025-05-23 10:07:54 +0200

    Update DP.py

--- Dynamic Programming/DP.py
+++ Dynamic Programming/DP.py
@@ -219,6 +219,21 @@ each unique (m, n) is calculated once, and reused.
 
 """
 
+"""
+
+Memoization Recipe
+1. Make it work.
+• visualize the problem as a tree
+• implement the tree using recursion
+• test it
+2. Make it efficient.
+• add a memo object
+• add a base case to return memo values
+• store return values into the memo
+
+
+"""
+
 
 
 
